59e54d04315111ba9c7238ebe74d3ea09eeb113d
Aryan Kashyap <aryankashyapbanka2003@gmail.com>
2025-12-04 20:07:43 +0530

    Setup server in go using chi router

---
+++ app/application.go
@@ -0,0 +1,29 @@
+package app
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+type Config struct {
+	Addr string
+}
+
+type Application struct {
+	Config Config
+}
+
+func (app *Application) Run() error {
+	server := &http.Server{
+		Addr:         app.Config.Addr,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		Handler:      nil,
+	}
+
+	fmt.Println("Starting server on", app.Config.Addr)
+
+	return server.ListenAndServe()
+}
+
